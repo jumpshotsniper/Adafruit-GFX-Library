882a12f986f94cb9a67a478542bcd85a19b1d976
FI-wind <82193919+FI-wind@users.noreply.github.com>
2021-12-26 00:45:50 +0800

    Update Adafruit_GFX.h

--- Adafruit_GFX.h
+++ Adafruit_GFX.h
@@ -40,6 +40,7 @@ class Adafruit_GFX : public Print {
   // optimized code.  Otherwise 'generic' versions are used.
   virtual void
     // It's good to implement those, even if using transaction API
+    drawEllipse(int16_t x1, int16_t y1, int16_t x2, int16_t y2, int16_t a, uint16_t color),
     drawFastVLine(int16_t x, int16_t y, int16_t h, uint16_t color),
     drawFastHLine(int16_t x, int16_t y, int16_t w, uint16_t color),
     fillRect(int16_t x, int16_t y, int16_t w, int16_t h, uint16_t color),
@@ -55,8 +56,6 @@ class Adafruit_GFX : public Print {
     fillCircle(int16_t x0, int16_t y0, int16_t r, uint16_t color),
     fillCircleHelper(int16_t x0, int16_t y0, int16_t r, uint8_t cornername,
       int16_t delta, uint16_t color),
-	  drawPentagram(int16_t x0, int16_t y0, int16_t r0, uint16_t color),
-	 drawEllipse(int16_t x1, int16_t y1, int16_t x2, int16_t y2, int16_t a, uint16_t color),
     drawTriangle(int16_t x0, int16_t y0, int16_t x1, int16_t y1,
       int16_t x2, int16_t y2, uint16_t color),
     fillTriangle(int16_t x0, int16_t y0, int16_t x1, int16_t y1,
@@ -84,7 +83,6 @@ class Adafruit_GFX : public Print {
       int16_t w, int16_t h),
     drawGrayscaleBitmap(int16_t x, int16_t y,
       uint8_t *bitmap, uint8_t *mask, int16_t w, int16_t h),
-    drawPentagram(int16_t x0, int16_t y0, int16_t r0, uint16_t color),
     drawRGBBitmap(int16_t x, int16_t y, const uint16_t bitmap[],
       int16_t w, int16_t h),
     drawRGBBitmap(int16_t x, int16_t y, uint16_t *bitmap,
@@ -94,8 +92,6 @@ class Adafruit_GFX : public Print {
       int16_t w, int16_t h),
     drawRGBBitmap(int16_t x, int16_t y,
       uint16_t *bitmap, uint8_t *mask, int16_t w, int16_t h),
-    drawEllipse(int16_t x1, int16_t y1,
-		int16_t x2, int16_t y2, int16_t a, uint16_t color),
     drawChar(int16_t x, int16_t y, unsigned char c, uint16_t color,
       uint16_t bg, uint8_t size),
     setCursor(int16_t x, int16_t y),
